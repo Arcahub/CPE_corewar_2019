fb6f15baa01aaf8916fefe82ab2409b412c4b015
Arcarox <arcarox@localhost.localdomain>
2020-05-05 14:39:24 +0200

    Refactor compile to handle special case

---
+++ asm/include/instructs/cw_asm_instruct_compile.h
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2020
+** CPE_corewar_2019
+** File description:
+** cw_asm_instruct_compile
+*/
+
+#ifndef CW_ASM_INSTRUCT_COMPILE_H_
+#define CW_ASM_INSTRUCT_COMPILE_H_
+
+#include "instructs/cw_asm_instruct.h"
+
+void cw_asm_instruct_compile_global(cw_asm_instruct_t *instruct);
+void cw_asm_instruct_compile_live(cw_asm_instruct_t *instruct);
+void cw_asm_instruct_compile_sti_ldi(cw_asm_instruct_t *instruct);
+void cw_asm_instruct_compile_zjump(cw_asm_instruct_t *instruct);
+void cw_asm_instruct_compile_fork(cw_asm_instruct_t *instruct);
+void cw_asm_instruct_compile_lfork(cw_asm_instruct_t *instruct);
+
+static void (*CW_ASM_INSTRUCTS_COMPILE_FP[])(cw_asm_instruct_t *) = {
+    &cw_asm_instruct_compile_live,
+    &cw_asm_instruct_compile_global,
+    &cw_asm_instruct_compile_global,
+    &cw_asm_instruct_compile_global,
+    &cw_asm_instruct_compile_global,
+    &cw_asm_instruct_compile_global,
+    &cw_asm_instruct_compile_global,
+    &cw_asm_instruct_compile_global,
+    &cw_asm_instruct_compile_zjump,
+    &cw_asm_instruct_compile_sti_ldi,
+    &cw_asm_instruct_compile_sti_ldi,
+    &cw_asm_instruct_compile_fork,
+    &cw_asm_instruct_compile_global,
+    &cw_asm_instruct_compile_global,
+    &cw_asm_instruct_compile_lfork,
+    &cw_asm_instruct_compile_global,
+};
+#endif /* !CW_ASM_INSTRUCT_COMPILE_H_ */
---
+++ asm/src/cw_asm_instructs/cw_asm_instructs_compile/cw_asm_instruct_compile_global.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2020
+** CPE_corewar_2019
+** File description:
+** cw_asm_instruct_compile_global
+*/
+
+#include "instructs/cw_asm_instruct.h"
+
+void cw_asm_instruct_compile_global(cw_asm_instruct_t *instruct)
+{
+    for (int i = 0; instruct->parameters[i]; i++) {
+        switch (*instruct->parameters[i]) {
+        case 'r':
+            instruct->coding_byte += 0b01;
+            instruct->instruct_size += 1;
+            break;
+        case DIRECT_CHAR:
+            instruct->coding_byte += 0b10;
+            instruct->instruct_size += DIR_SIZE;
+            break;
+        default:
+            instruct->coding_byte += 0b11;
+            instruct->instruct_size += IND_SIZE;
+        }
+        instruct->coding_byte = instruct->coding_byte << 2;
+    }
+    instruct->instruct_size += 2;
+}
