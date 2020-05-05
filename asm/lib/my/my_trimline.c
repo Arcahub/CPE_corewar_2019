8ef39ae0d9fc2610577878a4396e87ebb1228aa4
Arcarox <arcarox@localhost.localdomain>
2020-05-05 16:54:20 +0200

    Add function to clear line

---
+++ asm/lib/my/my_trimline.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2020
+** CPE_corewar_2019
+** File description:
+** my_trimline
+*/
+
+#include "my.h"
+
+char *my_trimline(char *line)
+{
+    char *res = NULL;
+    int j = 0;
+
+    res = malloc(sizeof(char) * (my_strlen(line) + 1));
+    for (int h = 0; h < my_strlen(line) + 1; h++)
+        res[h] = '\0';
+    if (res == NULL)
+        return (NULL);
+    for (int i = 0; line[i]; i++)
+        if (!(line[i] == ' ' || line[i] == '\t')) {
+            res[j] = line[i];
+            j++;
+        }
+    return (res);
+}
