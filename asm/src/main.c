e50cbbcc86d6fe73ef288f09d72b43f423cb2702
TempoDev <alexia.legrand@epitech.eu>
2020-05-08 19:25:18 +0200

    Add assembler usage (-h)

--- asm/src/main.c
+++ asm/src/main.c
@@ -5,9 +5,24 @@
 ** main
 */
 
+#include <unistd.h>
 #include "cw_asm.h"
+#include "my.h"
+
+static int cw_asm_display_usage(void)
+{
+    write(1, "USAGE\n", 6);
+    write(1, "./asm file_name[.s]\n", 20);
+    write(1, "DESCRIPTION\n", 12);
+    write(1, "file_name file in assembly language to be converted into" \
+             " file_name.cor, an\n", 75);
+    write(1, "executable in the Virtual Machine.\n", 35);
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
+    if (argc == 2 && !my_strcmp("-h", argv[1]))
+        return cw_asm_display_usage();
     return (cw_asm(argc, argv));
 }
