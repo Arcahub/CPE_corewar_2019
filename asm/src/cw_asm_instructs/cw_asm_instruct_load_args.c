c7f900883c7a31569e91a0f70f24aa23e9f9f1e5
Arcarox <arcarox@localhost.localdomain>
2020-05-08 16:39:13 +0200

    Fix unhandled comment that broke code

--- asm/src/cw_asm_instructs/cw_asm_instruct_load_args.c
+++ asm/src/cw_asm_instructs/cw_asm_instruct_load_args.c
@@ -9,6 +9,22 @@
 #include "my.h"
 #include <stdlib.h>
 
+static char *cw_asm_instruct_clean_arg(char *arg)
+{
+    char *str = NULL;
+    int i = 0;
+
+    for (; arg[i] != '\0' && arg[i] != COMMENT_CHAR; i++);
+    if (arg[i] != '\0') {
+        arg = my_strndup(arg, i);
+        str = arg;
+    }
+    arg = my_trimline(arg);
+    if (str)
+        free(str);
+    return (arg);
+}
+
 void cw_asm_instruct_load_args(cw_asm_instruct_t *instruct, char **line)
 {
     char **args = str_to_word_array_sep(*line, SEPARATOR_CHAR);
@@ -25,7 +41,7 @@ void cw_asm_instruct_load_args(cw_asm_instruct_t *instruct, char **line)
         return;
     }
     for (i = 0; i < nbr_args; i++) {
-        instruct->parameters[i] = my_trimline(args[i]);
+        instruct->parameters[i] = cw_asm_instruct_clean_arg(args[i]);
         free(args[i]);
     }
     free(args);
