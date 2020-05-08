344ac1abaf4b82a25cc25de469416410309c61a7
nasso <nassomails@gmail.com>
2020-05-08 21:09:59 +0200

    Add template project for ASM

---
+++ asm_template/src/main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** CPE_corewar_2019
+** File description:
+** Program entry point
+*/
+
+#include "my/my.h"
+#include "my/io.h"
+
+int main(void)
+{
+    my_printf("Hello World!\n");
+    return (0);
+}
