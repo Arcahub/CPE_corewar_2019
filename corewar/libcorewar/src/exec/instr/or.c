c722e262f64b563671d2ea20b1a98f78fc506ee7
nasso <nassomails@gmail.com>
2020-05-14 03:50:55 +0200

    Add or and xor

--- corewar/libcorewar/src/exec/instr/or.c
+++ corewar/libcorewar/src/exec/instr/or.c
@@ -11,8 +11,11 @@
 
 void cw_vm__exec__or(cw_vm_t *vm, cw_core_t *core, const cw_instr_t *instr)
 {
-    (void)(vm);
-    (void)(core);
-    (void)(instr);
+    u64_t a = cw_vm__exec_pval(vm, core, &instr->args[0], vm->config.reg_size);
+    u64_t b = cw_vm__exec_pval(vm, core, &instr->args[1], vm->config.reg_size);
+    u64_t or = a | b;
+
+    core->regs.zero = or == 0;
+    core->regs.regs[instr->args[2].u.reg] = or;
     core->regs.pc = instr->end;
 }
