eb1577d7310397db72d31bcae9cec430054173d9
nasso <nassomails@gmail.com>
2020-05-14 03:47:08 +0200

    Add zero flag to and + libmy

--- corewar/libcorewar/src/exec/instr/and.c
+++ corewar/libcorewar/src/exec/instr/and.c
@@ -15,6 +15,7 @@ void cw_vm__exec__and(cw_vm_t *vm, cw_core_t *core, const cw_instr_t *instr)
     u64_t b = cw_vm__exec_pval(vm, core, &instr->args[1], vm->config.reg_size);
     u64_t and = a & b;
 
+    core->regs.zero = and == 0;
     core->regs.regs[instr->args[2].u.reg] = and;
     core->regs.pc = instr->end;
 }
