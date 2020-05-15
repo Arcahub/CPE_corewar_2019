c41f4e222a16b4eb66a84a1abaad8d04913c6434
nasso <nassomails@gmail.com>
2020-05-15 17:56:49 +0200

    Fix integer sign when reading/writing ints

--- corewar/libcorewar/src/rw_int.c
+++ corewar/libcorewar/src/rw_int.c
@@ -11,14 +11,16 @@
 
 i64_t cw_vm__read_int(const cw_vm_t *vm, i64_t addr, usize_t n)
 {
+    bool sign = vm->mem[addr % vm->config.mem_size] & 0x80;
     union {
         i64_t i64;
         u8_t bytes[8];
-    } val = {0};
+    } val = { .i64 = sign ? -1 : 0 };
 
     for (usize_t i = 0; i < n; i++)
-        val.bytes[i] = vm->mem[(addr + i) % vm->config.mem_size];
-    return (u64_be_to_ne(val.i64) >> (64 - 8 * n));
+        val.bytes[8 - n + i] = vm->mem[(addr + i) % vm->config.mem_size];
+    val.i64 = u64_be_to_ne(val.i64);
+    return (val.i64);
 }
 
 void cw_vm__write_int(cw_vm_t *vm, i64_t val, i64_t addr, usize_t n)
@@ -28,7 +30,7 @@ void cw_vm__write_int(cw_vm_t *vm, i64_t val, i64_t addr, usize_t n)
         u8_t bytes[8];
     } u;
 
-    u.i64 = u64_ne_to_be(val << (64 - 8 * n));
+    u.i64 = u64_ne_to_be(val);
     for (usize_t i = 0; i < n; i++)
-        vm->mem[(addr + i) % vm->config.mem_size] = u.bytes[i];
+        vm->mem[(addr + i) % vm->config.mem_size] = u.bytes[8 - n + i];
 }
