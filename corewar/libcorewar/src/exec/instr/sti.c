4c9c1d630dc209792c543726883004f114e33917
nasso <nassomails@gmail.com>
2020-05-15 02:45:57 +0200

    Fix various instruction signedness

--- corewar/libcorewar/src/exec/instr/sti.c
+++ corewar/libcorewar/src/exec/instr/sti.c
@@ -12,20 +12,18 @@
 
 void cw_vm__exec__sti(cw_vm_t *vm, cw_core_t *core, const cw_instr_t *instr)
 {
-    u64_t a = cw_vm__exec_pval(vm, core, &instr->args[1], vm->config.ind_size);
-    u64_t b = cw_vm__exec_pval(vm, core, &instr->args[2], vm->config.ind_size);
+    i16_t a = cw_vm__exec_pval(vm, core, &instr->args[1], vm->config.ind_size);
+    i16_t b = cw_vm__exec_pval(vm, core, &instr->args[2], vm->config.ind_size);
+    u64_t size = u64_min(vm->config.reg_size, 8);
+    u64_t dst = core->regs.pc + (a + b) % vm->config.idx_mod;
     union {
         u64_t u64;
         u8_t bytes[8];
     } val = {0};
-    u64_t size = u64_min(vm->config.reg_size, 8);
-    u64_t max = core->regs.pc + (a + b) % vm->config.idx_mod + size;
-    u64_t j = 0;
 
     val.u64 = cw_vm__exec_pget(core, &instr->args[0]);
-    val.u64 = u64_ne_to_be(val.u64);
-    for (u64_t i = core->regs.pc + (a + b) % vm->config.idx_mod; i < max;
-        i++, j++)
-        vm->mem[i % vm->config.mem_size] = val.bytes[j];
+    val.u64 = u64_ne_to_be(val.u64 << (64 - 8 * vm->config.reg_size));
+    for (u64_t i = 0; i < size; i++)
+        vm->mem[(dst + i) % vm->config.mem_size] = val.bytes[i];
     core->regs.pc = instr->end;
 }
