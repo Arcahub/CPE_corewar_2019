4334dca04186e04642da2cdc1cc010a64505ef25
NoOverflow <julien.prost@epitech.eu>
2020-05-18 03:41:10 +0200

    Fix UI Bug

--- bonus/gui/src/gui/update/update.c
+++ bonus/gui/src/gui/update/update.c
@@ -21,9 +21,11 @@ static void draw_header(cg_ui_t *ui)
 
 static void draw_selected_core(cg_ui_t *ui, cw_vm_t *vm)
 {
+    if (vm->cores->len <= ui->config.selected_core)
+        return;
     cw_core_t *core = vm->cores->data[ui->config.selected_core];
 
-    mvwprintw(ui->stats_win, 14 + vm->prog_count, 2, "Core [%d / %d] :", ui->config.selected_core, vm->cores->len);
+    mvwprintw(ui->stats_win, 14 + vm->prog_count, 2, "Core [< %d / %d >] :", ui->config.selected_core, vm->cores->len);
     mvwprintw(ui->stats_win, 15 + vm->prog_count, 6, "Program Count  | [%d]", core->regs.pc);
     mvwprintw(ui->stats_win, 16 + vm->prog_count, 6, "Current OpCode | [%d]", vm->mem[core->regs.pc % vm->config.mem_size]);
 }
@@ -33,8 +35,8 @@ static void draw_stats(cg_ui_t *ui, cw_vm_t *vm)
     usize_t max_prog_name_len = 1;
 
     draw_header(ui);
-    mvwprintw(ui->stats_win, 9, 2, "Total Cycles    : [ % -3ld ]", vm->state.cycles);
-    mvwprintw(ui->stats_win, 10, 2, "Loaded Programs : [ % -3ld ]", vm->prog_count);
+    mvwprintw(ui->stats_win, 9, 2, "Total Cycles       : [ % -3ld ]", vm->state.cycles);
+    mvwprintw(ui->stats_win, 10, 2, "Loaded Programs    : [ % -3ld ]", vm->prog_count);
     for (size_t i = 0; i < vm->prog_count; i++)
         if (my_cstrlen(vm->programs[i].name) > max_prog_name_len)
             max_prog_name_len = my_cstrlen(vm->programs[i].name);
